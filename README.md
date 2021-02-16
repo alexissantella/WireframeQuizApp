bfe7562d88b03c2fb84e9cf19d38f601cde85793
Alexis Santella <69594663+alexissantella@users.noreply.github.com>
2021-02-16 15:09:34 -0500

    Submission for grading

--- README.md
+++ README.md
@@ -1,1 +1,139 @@
 ### Boilerplate app structure for Quiz App project
+
+function renderQA(){ //Render current question and matching answers to the form
+	var currentQ = STORE.currentQ;
+	$('#count').text('Question '+currentQ+' of '+(STORE.questions.length-2)); // Render question count
+	$('#question').text(STORE.questions[currentQ].q); // Render question as form legend text
+	$('#result').hide(); // Hide result DIV
+	initSelection();	// Select first radio button
+
+	if(currentQ == 0){ // INTRO
+		$('#count').css('visibility', 'hidden');
+		STORE.questions[currentQ].a.forEach(function(answer, index){ // Render answers to radio labels
+			var introHTML = `<p>${answer}</p>`;
+			$('#intro').append(introHTML);
+		});
+		$('.radio-item').hide();
+		$('#button-submit').show().text('Begin');
+		$('#button-continue').hide();
+	}
+	else if(currentQ == 11){ // END
+		$('#count').css('visibility', 'hidden');
+		getResults();
+		for (i = 1; i < STORE.questions.length-1; i++){ // Render answers to radio labels
+				var results = {
+					n: i,
+					q: STORE.questions[i].q,
+					c: STORE.questions[i].a[STORE.questions[i].c],
+					u: STORE.questions[i].a[STORE.questions[i].u],
+					r: STORE.questions[i].r
+				}
+			var resultsHTML = `<p class="results-p"><span class="results-number">${results.n}.</span> <span class="results-question">${results.q}</span><br>Correct Answer: <span class="results-correct">${results.c}</span><br>Your Answer: <span class="results-user">${results.u}</span><br>Result: <span class="results-result">${results.r}</span></p>`;
+			$('.radio-item').hide();
+			$('#end-results').hide();
+			$('#end-results').append(resultsHTML);
+			$('#end-results').fadeIn();
+			$('#button-submit').show().text('Restart');
+			$('#button-continue').hide();
+		};
+	}
+	else{
+		$('#count').css('visibility', 'visible');
+		$('#intro').hide();
+		$('.radio-item').fadeIn();
+		STORE.questions[currentQ].a.forEach(function(answer, index){ // Render answers to radio labels
+			$('label[for="answer-'+index+'"]').text(answer);
+		});
+		$('#button-submit').show().text('Submit');
+		$('#button-continue').hide();
+	}
+	console.log('CurrentQ is: '+currentQ);
+} 
+
+function initSelection(){ // CHECK FIRST RADIO AND GET IT AS THE CURRENT ANSWER
+	$('input[type=radio]:first').prop('checked', true);
+	getUserAnswer();
+}
+
+function startListeners(form){ // LISTEN TO FORM FOR ANSWER SELECTION AND SUBMIT
+	form.on('change','input[type=radio]', getUserAnswer)
+		.on('submit', handleSubmit);
+}
+
+function getUserAnswer(event){ // This gets the checked radio and stores it
+	STORE.currentUserAnswer = Number($('input:checked').val());
+	return STORE.currentUserAnswer;
+}
+
+function handleSubmit(event){ //When a fool smashes dat SUBMIT, please do the following
+	event.preventDefault();
+
+	if(STORE.currentQ == 11 ){ // If at end, submit will reload entire quiz
+		console.log('RESTART');
+		window.location.href='';
+	}
+	else if(STORE.currentQ == 0){
+		console.log('BEGIN');
+		renderQA(STORE.currentQ += 1); //Renders the new current page (next page)
+	}
+	else{
+		console.log('SUBMIT');
+		storeUserAnswer(getUserAnswer());
+		checkUserAnswer(getUserAnswer());
+	}
+}
+
+function storeUserAnswer(answer){ // Push stored User Answer into the answerHistory array
+	STORE.questions[STORE.currentQ].u = answer;
+}	
+
+function checkUserAnswer(userAnswer){ // check answer and push true if correct, false if incorrect
+	if(STORE.currentQ == 10){ // Any answer is correct for question 10
+		STORE.questions[10].a.push('Any'); // Push a new answer 'Any'
+		STORE.questions[10].c = 4;	// Ensure correct answer will be 'Any'
+		console.log('CORRECT!');
+		displayResult('CORRECT!');
+	}
+	else if(userAnswer == STORE.questions[STORE.currentQ].c){
+		STORE.questions[STORE.currentQ].r = 'Correct';
+		console.log('CORRECT!');
+		$('#result').removeClass('incorrect').addClass('correct');
+		displayResult('CORRECT!')
+	}
+	else {
+		STORE.questions[STORE.currentQ].r = 'Incorrect';
+		console.log('YA BLEW IT!');
+		$('#result').removeClass('correct').addClass('incorrect');
+		displayResult('YA BLEW IT!')
+	}
+}
+
+function displayResult(result){ // Show if User Answer is correct or not
+	$('.radio-item').hide();
+	$('#button-submit').hide();
+	$('#result').fadeIn();
+	$('#result > p').text(result);
+	$('#button-continue').show().unbind('click').click(function(e){
+		e.preventDefault();
+		console.log('currentq +1')
+		renderQA(STORE.currentQ += 1); //Renders the new current page (next page)
+
+	});
+}
+
+function getResults(){ // Match each user answer with appropriate question
+	var resultsArray=[];
+	for(i = 1 ; i < STORE.questions.length-1; i++){
+		var question = STORE.questions[i].q;
+		var answerNum = STORE.questions[i].u;
+		var answerStr = STORE.questions[i].a[answerNum];
+		resultsArray.push( {q:question, a:answerStr} );
+	}
+	return STORE.questions[11].a = resultsArray;
+}
+
+$(function(){ //DOCUMENT READY!
+	renderQA();
+	startListeners($('form'));
+	$('.no-fouc').removeClass('no-fouc');
+});
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Example store structure
  */
-const store = {
+/*const store = {
   // 5 or more questions are required
   questions: [
     {
@@ -28,7 +28,7 @@ const store = {
   quizStarted: false,
   questionNumber: 0,
   score: 0
-};
+};*/
 
 /**
  * 
@@ -56,3 +56,319 @@ const store = {
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+//document.documentElement.className = 'no-fouc'; // To prevent FOUC! - see the document ready for more
+
+const store = {
+  // 5 or more questions are required
+  questions: [
+    { // Question 1
+      question: 'What planet is the fastest in our solar system? This planet zips around our sun at an average of 107,000 miles per hour, about 40,000 mph faster than earth.',
+      answers: [
+        'Jupiter',
+        'Neptune',
+        'Mercury',
+        'Mars'
+      ],
+      correctAnswer: 'Neptune'
+    },
+    { // Question 2
+      question: 'How many Earths could you fit into Jupiter?',
+      answers: [
+        '2,300',
+        '500',
+        '800',
+        '1,300'
+      ],
+      correctAnswer: '1,300'
+    },
+    { // Question 3
+      question: 'Which planets winds are the fastest in the solar system? These winds reach 1,600 miles per hour, meaning these giant spinning storms could swallow the whole Earth.',
+      answers: [
+        'Jupiter',
+        'Neptune',
+        'Venus',
+        'Mars'
+      ],
+      correctAnswer: 'Neptune'
+    },
+    { // Question 4
+      question: 'The radio signal that a spacecraft uses to contact Earth has no more power than a ___ . And by the time the signal has traveled across space it is only measured at one-billionth of one-billionth of one watt.',
+      answers: [
+        'Refrigerator light bulb',
+        'Computer mouse',
+        'Cell phone',
+        'Alarm clock'
+      ],
+      correctAnswer: 'Refrigerator light bulb'
+    },
+    { // Question 5
+      question: 'The Largest canyon system in the solar system is Valles Marineris. It is more than 3,000 miles long and would stretch from California to New York. It is nine times as long and four times as deep as Earths Grand Canyon. Where is it?',
+      answers: [
+        'Saturn',
+        'Jupiter',
+        'Uranus',
+        'Mars'
+      ],
+      correctAnswer: 'Mars'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  submittingAnswer: false,
+  score: 0,
+
+  currentQuestionState: {
+    answerArray: []
+  }
+};
+
+
+function generateWelcomeString() {
+  return `
+  <div class="Welcome">
+    <form>
+      <p>
+        Welcome let\'s see if you know the unknown. Pay attention and don\'t space out. 
+      </p>
+      <p>
+        Have fun and may the force be with you!
+      </p>
+      <button type="submit" class="beginQuiz" autofocus>Begin Quiz</button>
+    </form>
+  </div>
+    `;
+}
+
+function generateQuizInterfaceString(questionObject) {
+  // console.log(questionObject);
+  // console.log(questionObject.question.answers);
+  return `
+    <div class='quiz-interface'>
+      <p class='outOf'>Question ${questionObject.index} out of ${store.questions.length}</p>
+      <p>
+       ${questionObject.question.question}
+      </p>
+
+      <form>
+      <ol style="list-style-type:none;">
+        ${generateQuizAnswers(questionObject.question.answers)}
+      </ol>
+      <button type="submit" class="submit-answer">Submit Answer</button>
+      </form> 
+      <p>Score: ${store.score}</p>
+    </div>
+    `;
+}
+
+
+function generateAnswerResults() {
+  let answerArray = store.currentQuestionState.answerArray;
+
+  const buttons = {
+    next: ' <button type="submit" class="next-question" autofocus>Next Question</button>',
+    results: '<button type="submit" class="see-results" autofocus>See Results</button>'
+  };
+
+  let correctResponse = `"${answerArray[1]}" is correct`;
+  let incorrectResponse = `${answerArray[2]} is not correct. The correct answer is<br><br>
+  <p  class="white-text">"${answerArray[1]}"</p>`;
+
+  let isLastQuestion = (store.questionNumber + 1) === (store.questions.length);
+
+  return `
+    <div class="answer-response">
+    <form>
+    <p>${answerArray[0] === true ? correctResponse : incorrectResponse}</p>
+    <p> Score: ${store.score}</p>
+   ${isLastQuestion ? buttons.results : buttons.next}
+    </form>
+    </div>
+  `;
+}
+
+
+function generateQuizAnswers(answers) {
+  let answerArray = [];
+  let indexArray = [];
+  answers.forEach(answer => {
+    answerArray.push(answer);
+    indexArray.push(answers.indexOf(answer));
+  });
+  console.log(indexArray);
+  return answerArray.map(answer => stringifyAnswerArray(answer)).join('');
+}
+
+function stringifyAnswerArray(answer) {
+  let questionNumber = store.questionNumber;
+  let name = store.questions[questionNumber].answers.indexOf(answer);
+  console.log(name);
+
+  return `
+    <li>
+      <div class="answer-container">
+      <input type="radio" name="answer" id="answer-${name}" data-answer="${answer}">
+      <label for="answer-${name}"> ${answer}</label>
+     
+      </div>
+    </li>
+  `;
+}
+
+function generateQuizResultsString() {
+  return `
+    <div class='quiz-results'>
+      <p>
+       Done. Great job, you're out of this world!
+      </p>
+          <p class="pink-text">You scored ${store.score} out of ${store.questions.length * 10}</p>            
+        <button class="restart-quiz">Restart Quiz</button>      
+    </div>   
+  `;
+}
+
+
+/********** RENDER FUNCTION(S) **********/
+
+function renderQuiz() {
+
+  if (store.quizStarted === false) {
+    if (store.questionNumber === store.questions.length) {
+      const quizResultsString = generateQuizResultsString();
+      $('main').html(quizResultsString);
+    } else {
+      const quizWelcomeInterfaceString = generateWelcomeString();
+      $('main').html(quizWelcomeInterfaceString);
+    }
+  } else if (store.quizStarted === true) {
+    if (store.submittingAnswer === false) {
+      const quizInterfaceString = generateQuizInterfaceString(currentQuestion());
+      $('main').html(quizInterfaceString);
+    } else if (store.submittingAnswer === true) {
+      const quizAnswerResponseString = generateAnswerResults();
+      $('main').html(quizAnswerResponseString);
+    }
+  }
+}
+
+
+// Changes the state of the application to a quizStarted = true
+function startQuiz() {
+  console.log('quiz has begun');
+  store.quizStarted = true;
+}
+
+// currentQuestion
+function currentQuestion() {
+  let index = store.questionNumber;
+  let questionObject = store.questions[index];
+  return {
+    index: index + 1,
+    question: questionObject
+  };
+}
+
+// Go to the next question of the quiz
+// Model function changes state
+function nextQuestion() {
+  if (store.questionNumber < store.questions.length) {
+    store.questionNumber++;
+    store.submittingAnswer = false;
+    console.log(store.questionNumber);
+  } else if (store.questionNumber === store.questions.length) {
+    store.quizStarted = false;
+  }
+}
+
+
+function validateCorrectAnswer() {
+  let radios = $('input:radio[name=answer]');
+  let selectedAnswer = $('input[name="answer"]:checked').data('answer');
+  let questionNumber = store.questionNumber;
+  let correctAnswer = store.questions[questionNumber].correctAnswer;
+
+  if (radios.filter(':checked').length === 0) {
+    alert('Please select an answer.');
+    return;
+  } else {
+    store.submittingAnswer = true;
+    if (selectedAnswer === correctAnswer) {
+      store.score += 10;
+      store.currentQuestionState.answerArray = [true, correctAnswer, selectedAnswer];
+    } else {
+      store.currentQuestionState.answerArray = [false, correctAnswer, selectedAnswer];
+    }
+  }
+}
+
+function seeResults() {
+  store.quizStarted = false;
+  store.questionNumber++;
+}
+
+function restartQuiz() {
+  store.quizStarted = false;
+  store.questionNumber = 0;
+  store.submittingAnswer = false;
+  store.currentQuestionState.answerArray = [];
+}
+
+/********** EVENT HANDLER FUNCTIONS **********/
+// These functions handle events (submit, click, etc)
+// Controller layer
+
+function handleBeginQuizSubmit() {
+
+  $('main').on('click', '.beginQuiz', (event) => {
+    event.preventDefault();
+    startQuiz();
+    renderQuiz();
+  });
+}
+
+function handleSubmitAnswer() {
+  $('main').on('click', '.submit-answer', (event) => {
+    event.preventDefault();
+    console.log('submitting answer');
+    validateCorrectAnswer();
+    renderQuiz();
+  });
+}
+
+function handleNextQuestionSubmit() {
+  $('main').on('click', '.next-question', (event) => {
+    event.preventDefault();
+    nextQuestion();
+    renderQuiz();
+  });
+}
+
+function handleSeeResultsSubmit() {
+  $('main').on('click', '.see-results', (event) => {
+    event.preventDefault();
+    seeResults();
+    renderQuiz();
+  });
+}
+
+function handleRestartQuizSubmit() {
+  $('main').on('click', '.restart-quiz', (event) => {
+    event.preventDefault();
+    restartQuiz();
+    renderQuiz();
+  });
+}
+
+
+// This function will launch all other functions after the page is loaded
+function handleQuiz() {
+  renderQuiz();
+  handleBeginQuizSubmit();
+  handleSubmitAnswer();
+  handleNextQuestionSubmit();
+  handleRestartQuizSubmit();
+  handleSeeResultsSubmit();
+
+}
+
+$(handleQuiz);
